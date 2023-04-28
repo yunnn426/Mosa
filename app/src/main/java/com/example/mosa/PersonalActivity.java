188c74c9a6f0cb64a69cbea349919dcf104714f7
윤석효 <dbstjrgy111@kyonggi.ac.kr>
2023-04-28 23:57:11 +0900

    결과화면 내에 추천아이템 리스트를 표시(뷰 관련 요소만 고치면 될듯!)

--- app/src/main/java/com/example/mosa/PersonalActivity.java
+++ app/src/main/java/com/example/mosa/PersonalActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,20 +13,33 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mosa.recommend.item_Recycler;
 import com.example.mosa.recommend.recaccActivity;
 import com.example.mosa.recommend.recclothActivity;
 import com.example.mosa.recommend.reccosActivity;
 import com.example.mosa.recommend.rechairActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PersonalActivity  extends AppCompatActivity {
 
@@ -50,6 +64,12 @@ public class PersonalActivity  extends AppCompatActivity {
     ImageView selected_color;
     FirebaseFirestore db;
     CollectionReference diagnosesref;
+    FirebaseStorage storage;
+    RecyclerView itemlist_1;
+    RecyclerView itemlist_2;
+    RecyclerView itemlist_3;
+    RecyclerView itemlist_4;
+    File path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +78,209 @@ public class PersonalActivity  extends AppCompatActivity {
         color_title=findViewById(R.id.skin_img);
         Intent intent=getIntent();
 
+        item_Recycler recycler_1=new item_Recycler();
+        item_Recycler recycler_2=new item_Recycler();
+        item_Recycler recycler_3=new item_Recycler();
+        item_Recycler recycler_4=new item_Recycler();
+
+        ArrayList<Bitmap> itemfile_1=new ArrayList<Bitmap>();
+        ArrayList<Bitmap> itemfile_2=new ArrayList<Bitmap>();
+        ArrayList<Bitmap> itemfile_3=new ArrayList<Bitmap>();
+        ArrayList<Bitmap> itemfile_4=new ArrayList<Bitmap>();
+
+        itemlist_1=findViewById(R.id.cos_list);
+        itemlist_2=findViewById(R.id.hair_list);
+        itemlist_3=findViewById(R.id.acc_list);
+        itemlist_4=findViewById(R.id.cloth_list);
+
+        storage=FirebaseStorage.getInstance();
+        StorageReference storageReference_1=storage.getReference().child(result_color[0]+"/cosmetics/"+"rip/");
+        StorageReference storageReference_2=storage.getReference().child(result_color[0]+"/cosmetics/"+"blusher/");
+        StorageReference storageReference_3=storage.getReference().child(result_color[0]+"/cosmetics/"+"shadow/");
+        StorageReference storageReference_4=storageReference_3;
+
+        File result_path= getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        storageReference_1.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                path=new File(result_path.getAbsolutePath());
+                for (StorageReference item : listResult.getItems()) {
+                    //다운로드 받을 파일의 이름을 지정
+                    File download=new File(path,item.getName());
+                    if(!path.exists()){
+                        path.getParentFile().mkdirs();
+                    }
+                    try {
+                        //파일을 생성
+                        download.createNewFile();
+                        //파일을 다운로드할 작업을 정의
+                        FileDownloadTask fileDownloadTask=item.getFile(download);
+                        fileDownloadTask.addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                //제대로 비트맵 이미지를 받아온 것을 확인, 실제 경로 상에 파일이 존재, 배열도 확인
+                                Bitmap bitmap=BitmapFactory.decodeFile(download.getAbsolutePath());
+                                itemfile_1.add(bitmap);
+                                if(itemfile_1.size()==4){
+                                    recycler_1.setrecycler(itemfile_1);
+                                    itemlist_1.setLayoutManager(new LinearLayoutManager(PersonalActivity.this, RecyclerView.HORIZONTAL, false));
+                                    itemlist_1.setAdapter(recycler_1);
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PersonalActivity.this,"스토리지를 불러오는 것을 실패 했습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        storageReference_2.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                path=new File(result_path.getAbsolutePath());
+                for (StorageReference item : listResult.getItems()) {
+                    //다운로드 받을 파일의 이름을 지정
+                    File download=new File(path,item.getName());
+                    if(!path.exists()){
+                        path.getParentFile().mkdirs();
+                    }
+                    try {
+                        //파일을 생성
+                        download.createNewFile();
+                        //파일을 다운로드할 작업을 정의
+                        FileDownloadTask fileDownloadTask=item.getFile(download);
+                        fileDownloadTask.addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                //제대로 비트맵 이미지를 받아온 것을 확인, 실제 경로 상에 파일이 존재, 배열도 확인
+                                Bitmap bitmap=BitmapFactory.decodeFile(download.getAbsolutePath());
+                                itemfile_2.add(bitmap);
+                                if(itemfile_2.size()==4 ){
+                                    recycler_2.setrecycler(itemfile_2);
+                                    itemlist_2.setLayoutManager(new LinearLayoutManager(PersonalActivity.this, RecyclerView.HORIZONTAL, false));
+                                    itemlist_2.setAdapter(recycler_2);
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PersonalActivity.this,"스토리지를 불러오는 것을 실패 했습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        storageReference_3.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                path=new File(result_path.getAbsolutePath());
+                for (StorageReference item : listResult.getItems()) {
+                    //다운로드 받을 파일의 이름을 지정
+                    File download=new File(path,item.getName());
+                    if(!path.exists()){
+                        path.getParentFile().mkdirs();
+                    }
+                    try {
+                        //파일을 생성
+                        download.createNewFile();
+                        //파일을 다운로드할 작업을 정의
+                        FileDownloadTask fileDownloadTask=item.getFile(download);
+                        fileDownloadTask.addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                //제대로 비트맵 이미지를 받아온 것을 확인, 실제 경로 상에 파일이 존재, 배열도 확인
+                                Bitmap bitmap=BitmapFactory.decodeFile(download.getAbsolutePath());
+                                itemfile_3.add(bitmap);
+                                if(itemfile_3.size()==4){
+                                    recycler_3.setrecycler(itemfile_3);
+                                    itemlist_3.setLayoutManager(new LinearLayoutManager(PersonalActivity.this, RecyclerView.HORIZONTAL, false));
+                                    itemlist_3.setAdapter(recycler_3);
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PersonalActivity.this,"스토리지를 불러오는 것을 실패 했습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        storageReference_4.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                path=new File(result_path.getAbsolutePath());
+                for (StorageReference item : listResult.getItems()) {
+                    //다운로드 받을 파일의 이름을 지정
+                    File download=new File(path,item.getName());
+                    if(!path.exists()){
+                        path.getParentFile().mkdirs();
+                    }
+                    try {
+                        //파일을 생성
+                        download.createNewFile();
+                        //파일을 다운로드할 작업을 정의
+                        FileDownloadTask fileDownloadTask=item.getFile(download);
+                        fileDownloadTask.addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                //제대로 비트맵 이미지를 받아온 것을 확인, 실제 경로 상에 파일이 존재, 배열도 확인
+                                Bitmap bitmap=BitmapFactory.decodeFile(download.getAbsolutePath());
+                                itemfile_4.add(bitmap);
+                                if(itemfile_4.size()==4){
+                                    recycler_4.setrecycler(itemfile_4);
+                                    itemlist_4.setLayoutManager(new LinearLayoutManager(PersonalActivity.this, RecyclerView.HORIZONTAL, false));
+                                    itemlist_4.setAdapter(recycler_4);
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PersonalActivity.this,"스토리지를 불러오는 것을 실패 했습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         String image_path=intent.getStringExtra("img");
         File file=new File(image_path);
