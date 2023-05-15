009d00520c946c3c960a425e444c73d5111560c6
yunnn426 <lucia_hwang@naver.com>
2023-05-16 00:34:32 +0900

    다시 커밋

--- app/src/main/java/com/example/mosa/MainActivity.java
+++ app/src/main/java/com/example/mosa/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //git pull request test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
