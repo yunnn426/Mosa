16787df999d4ac1399a16bd3952252d6574df2cc
hokyun <wjdghrbs313@kyonggi.ac.kr>
2023-05-30 20:14:21 +0900

    사진 회전 및 사이즈 조정 오류 수정

--- app/src/main/java/com/example/mosa/IntitialActivity.java
+++ app/src/main/java/com/example/mosa/IntitialActivity.java
@@ -7,8 +7,6 @@ import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 
-import android.content.ContentResolver;
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -16,10 +14,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.ExifInterface;
-import android.media.Image;
+import androidx.exifinterface.media.ExifInterface;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -28,21 +24,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -237,6 +226,20 @@ public class IntitialActivity extends AppCompatActivity {
         }
         return file;
     }
+    public String getRealPathFromURI(Uri contentUri){
+        String result;
+
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+        if(cursor==null) {
+                result = contentUri.getPath();
+        } else{
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -250,8 +253,12 @@ public class IntitialActivity extends AppCompatActivity {
                         Bitmap img = BitmapFactory.decodeStream(in);
                         Uri uri = data.getData();
                         in.close();
+                        Uri photoUri = Uri.parse(getRealPathFromURI(uri));
+                        exif = new ExifInterface(photoUri.getPath());
+                        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
                         Bitmap resize_bitmap = resize_imageSize(this, uri, img, "image.png");
-                        btn3.setImageBitmap(resize_bitmap);
+                        Bitmap rotate = rotateBitmap(resize_bitmap,orientation);
+                        btn3.setImageBitmap(rotate);
                     }catch (Exception e){
 
                     }
